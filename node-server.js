f87d071332e955fcb65b06aabb887615b2d2e9cc
ShopMateServer <shopmatesales@gmail.com>
2025-04-19 16:18:52 +0200

    latest

--- node-server.js
+++ node-server.js
@@ -10,7 +10,7 @@ const uri = "mongodb://localhost:27017"
 const {upload} = import('express-fileupload');
 const{fs} = import('fs')
 
-const { MongoClient } = import('mongodb');
+const { MongoClient } = require('mongodb');
  
  // Enable command monitoring for debugging
 const mongoClient = new MongoClient('mongodb://localhost:27017', { monitorCommands: true });
