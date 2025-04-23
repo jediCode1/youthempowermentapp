51e5935eedf0e946a0f86e3ae4b8a7e7563f7c1b
ShopMateServer <shopmatesales@gmail.com>
2025-04-24 01:10:15 +0200

    latest

--- node-server.js
+++ node-server.js
@@ -1,4 +1,4 @@
-const path = import('path') 
+import path from 'path'
 import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
